4c9553de575505e11cec3c6e15cdf1a8a3bc33cf
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-08 13:34:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/' do
     "Welcome to your app!!!! "
+  end
+  get '/' do
+  "Welcome to your app!!!! I BUILT THIS!"
   end
 
 end
